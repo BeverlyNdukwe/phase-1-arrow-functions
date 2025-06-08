c92234e1a0c1dd9e81b927cb47d84557e182e281
Beverly Ndukwe <ndukwebeverly.com>
2025-06-08 12:47:28 +0300

    Initialize commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(a, b) {
+  return a / b;
+};
+
+// Arrow function called square
+const square = (x) => {
+  return x * x;
+};
+
+// Arrow function called add
+const add = (a, b) => {
+  return a + b;
+};
